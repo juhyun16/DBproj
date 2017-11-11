6974128659edbf9e64a022b75af654c4de066548
Juhyun Park <juhyun1849@gmail.com>
2017-11-11 20:58:35 +0900

    settings.py에 DB환경설정하고 requirements.txt도 만듦

--- settings/settings.py
+++ settings/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'DBproj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'database2017',
+        'HOST': '165.132.105.47',
+        'PORT': '3306',
+        'USER': 'database2017',
+        'PASSWORD': 'db20170101',
     }
 }
 
